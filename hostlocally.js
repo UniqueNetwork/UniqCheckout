79274cfcf5c4173fe21b247b9f47d835fca2954b
stanta <st.takt@gmail.com>
2021-12-17 15:54:50 +0300

    fixed public key transfer, added payment flows conditions

--- hostlocally.js
+++ hostlocally.js
@@ -11,65 +11,87 @@ const clientSec = process.env.clientSec || "Pmg36sDWQ9WxtPR3"
 const clientSk = process.env.sk || "sk_sbox_vueg7yv6ibajwcjbvxp7mfdzgqe" //'sk_test_3e1ad21b-ac23-4eb3-ad1f-375e9fb56481'
 const clientpk = process.env.pk ||  "pk_sbox_6e4nist6o5uenuq6ei5dithevqt"
 const { Checkout } = require('checkout-sdk-node');
-
+const {sendNFT} = require("./marketplace")
 
 
 app.use(express.static('./public'));
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 var jsonParser = bodyParser.json()
 
-app.post('/checkout', jsonParser,  async function (req, res) {
+
+app.get('/', (req, res) => {
+  console.log (clientpk)
+  res.render('index', {
+    cckk: clientpk,
+  });
+})
+
+app.post('/checkout/:contractNFT/:tokenID', jsonParser,  async function (req, res) {
 
     const cko = new Checkout(clientSk, {pk:clientpk, timeout: 7000});
 
     (async () => {
 
-      //autentification token
+      //autentification token request 
       try {
         const transaction = await cko.payments.request({
             source:  {
-               type: "token",
+              type: "token",
               token: req.body.token 
 /*                 number: '4242424242424242',
                 expiry_month: '12', // req.body.expiry_month,
                 expiry_year: '23', //req.body.expiry_year,
                 cvv: '100' */
             } ,
+/*             customer: {
+              email: 'user@email.com',
+              name: 'James Bond',
+          }, */
             currency:  req.body.currency,
             amount: req.body.amount,
-            capture: "false",
-         //   capture_type : "final" ,
+    //        capture: "false",
+            capture_type : "final" ,
             reference: "123456" // order ID
+            // https://api-reference.checkout.com/preview/crusoe/#operation/requestAPaymentOrPayout 
         });
-        console.log(transaction);
+        console.log(transaction.id);
+        const details = await cko.payments.get(transaction.id); // or with session id sid_XX
+
+        console.log("details", details)
+        const actions = await cko.payments.getActions(transaction.id);
+        console.log("actions", actions)
+
         
+        if (transaction.status === 'Pending') {
+          // The payment is 3DS. Redirect the customer to payment.redirectLink
+          sendNFT(req.params.contractNFT, req.params.tokenID)
+        } else if (transaction.approved == true && risk.flagged == false) {
+          // The payment was successful and not flagged by any risk rule
+        } else if (transaction.approved == true && transaction.risk.flagged == true) {
+          // The payment was successful but it was flagged by a risk rule; this means you have to manually decide if you want to capture it or void it
+        } else if (transaction.approved == false) {
+          // the payment was declined
+        }
+         
         // call market   to accepted deposite 
         // call market to send NFT
 
       // if all ok       
       //finalizing payment
-      //token request 
-      const transaction = await cko.payments.request({
-        source:  {
-           type: "token",
-          token: req.body.token 
-/*                 number: '4242424242424242',
-            expiry_month: '12', // req.body.expiry_month,
-            expiry_year: '23', //req.body.expiry_year,
-            cvv: '100' */
-        } ,
-        currency:  req.body.currency,
-        amount: req.body.amount,
-        //capture: "false",
-        capture_type : "final" ,
-        reference: "123456" // order ID
-      //else 
-      // uncapture money
-    // revoke escrow (?)
+      const transaction2 = await cko.payments.capture( transaction.id);
+      // https://api-reference.checkout.com/preview/crusoe/#tag/Payments/paths/~1payments~1{id}~1captures/post
+    console.log("capture ", transaction2);
+      
+    const detailsCap = await cko.payments.get(transaction.id); // or with session id sid_XX
 
-      });
+    console.log("detailsCap", detailsCap)
+
+
+    
+  }
       catch (err) {
         console.log(err)
       }
